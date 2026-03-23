25762b8e881dd2d83f3c1e5f2731e565f8b405ee
allghz16 <andiandika3608>
2026-03-23 12:02:22 +0700

    initial react project

--- mulyono/src/App.js
+++ mulyono/src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import dhika from "./pages/dhika";
-import haoiz from "./pages/hapiz";
-import zaki from "./pages/zaki";
-import james from "./pages/james";
-import navbar from "./components/navbar";
+
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Dhika from "./pages/dhika";
 
 function App() {
   return (
@@ -13,9 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dhika-alghazali" element={<Dhika />} />
-        <Route path="/james" element={<James />} />
-        <Route path="/hapiz" element={<Hapiz />} />
-        <Route path="/zaki" element={<Zaki />} />
       </Routes>
     </Router>
   );
